d7801a96988552bbb2bbb0317f319200bde4828f
Aitor Ruiz <aitorruiz2m@gmail.com>
2022-01-10 17:13:07 +0100

    Chache delete

--- hooks/useGetLogs.js
+++ hooks/useGetLogs.js
@@ -1,7 +1,7 @@
 import useSWR from 'swr'
 
 export default function useUser (id) {
-    const { data, error } = useSWR(`/api/getData/${id}`)
+    const { data, error } = useSWR(`/api/getData/${id}?_vercel_no_cache=1`)
   
     return {
       logs: data,
